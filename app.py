c768b850e163c94a6d555581ace9ace0efbe4548
Edgar Jonathan Martínez Rodríguez <mrodrige@telmex.com>
2020-06-15 18:55:53 -0500

    Integración test

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello world version 2'
 
+@app.route('/test')
+def test():
+    return 'Hello test'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8030')
